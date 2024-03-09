f1de959ad59772843ee96c8546f58d99512f6ffe
Sereny <serejke33@gmail.com>
2024-03-09 21:58:18 +0300

    add createGrid function
    create grid of divs of NxN size
    stack every div into a container

---
+++ script.js
@@ -0,0 +1,27 @@
+/* 
+    1. Create grid of divs. (should be evenly spread with flex)
+    2. Input is - mouse hover event Listener
+        each div changes color on hover;   
+    Output is - a sketch
+        
+    3. 'clear' button should return the grid to white.
+
+*/
+
+// Create grid of divs
+
+const grid = document.querySelector('#container');
+
+function createGrid() {
+    let a = +prompt('Write your grid size', 0)
+    let gridSize = a * a;
+    for (let i = 0; i<gridSize; i++) {
+        const div = document.createElement('div');
+        div.classList.toggle('div')
+        grid.appendChild(div);
+        console.log('appendChild')
+    }
+}
+
+
+
