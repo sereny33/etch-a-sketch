2e48677921895620b2fa041e175587efaf360e8a
Sereny <serejke33@gmail.com>
2024-03-10 23:19:41 +0300

    add draw function
    turns squares black when mouse over them

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let a = +prompt('Write the grid size', '')
 const gridSize = a*a;
 // calculate proper square size for 500x500 px container
 const squareSize = (Math.sqrt((250000/gridSize))); 
-
+const squareDivArray = [];
 function createGrid(gridSize) {
     
     for (i = 0; i<gridSize; i++) {
@@ -13,8 +13,15 @@ function createGrid(gridSize) {
         squareDiv.classList.add('squareDiv');
         squareDiv.style.width = `${squareSize}px`;
         squareDiv.style.height = `${squareSize}px`;
-        
-        gridContainer.appendChild(squareDiv)
-        
+        gridContainer.appendChild(squareDiv);
+        squareDivArray.push(squareDiv);
     }
+// add event listeners to each squareDiv
+    squareDivArray.forEach((div) => {
+        div.addEventListener("mouseover", (e) => {
+            e.target.style.background = "black";
+        });
+    });
 }
+
+createGrid(gridSize);
