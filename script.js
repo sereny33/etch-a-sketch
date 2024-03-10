1ef87197b4049db2e5da4174e0a2da8e60efe1c6
Sereny <serejke33@gmail.com>
2024-03-11 01:07:50 +0300

    add grid-resize function
    empty existing grid and get new user input;
    fix square size behaviour;

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
-// fill the container with properly sized squares
-
 const gridContainer = document.querySelector('.container');
-let a = +prompt('Write the grid size', '')
-const gridSize = a*a;
 
-const squareSize = (Math.sqrt((250000/gridSize))); // calculate proper square size for 500x500 px container
-const squareDivArray = [];
+let gridSize;
+// ask for grid size
+function getGridSize() {
+    let a;
+    do {
+        a = +prompt('Write the grid size 16-100', '');
+    } while (a<16 || a>100);
+    return a*a;
+}
+
+// fill the container with properly sized squares
+let squareDivArray = [];
 function createGrid(gridSize) {
-    
+    let squareSize = (Math.sqrt((500*500/gridSize))); // calculate proper square size for 500x500 px container
     for (i = 0; i<gridSize; i++) {
         const squareDiv = document.createElement('div');
         squareDiv.classList.add('squareDiv');
@@ -18,21 +24,25 @@ function createGrid(gridSize) {
     }
 // add event listeners to each squareDiv
     squareDivArray.forEach((div) => {
-        div.addEventListener("mouseover", (e) => {
+        div.addEventListener("mouseenter", (e) => {
             e.target.style.background = "black";
         });
     });
 }
 
-createGrid(gridSize);
-
 // clear drawing area
-
 const clearBtn = document.querySelector('#clearBtn');
 clearBtn.addEventListener("click", clearArea)
 
 function clearArea() {
     squareDivArray.forEach((div) => {
-        div.style.background = "pink"
-    })
-}
+        div.style.background = "pink";
+    });
+};
+
+// clear gridArea and resize it
+const newGridBtn = document.querySelector('#newGrid');
+newGridBtn.addEventListener("click", () => {
+    gridContainer.textContent = '';
+    createGrid(getGridSize());
+});
